501d753784624e5edc82a1ab13cfd445cc092f92
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2024-07-11 05:12:38 +0100

    prettier

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
-import type { RouteObject } from 'react-router-dom';
+import type { RouteObject } from "react-router-dom";
 import Index from "../pages";
+import NewQuote from "../pages/newQuote";
+import EditDraftQuote from "../pages/editDraftQuote";
+import EditQuote from "../pages/editQuote";
 
 const routes: RouteObject[] = [
-    {
-        path: '/',
-        element: <Index />,
-        // errorElement: </>,
-    },
+  {
+    path: "/",
+    element: <Index />,
+    // errorElement: </>,
+  },
+  {
+    path: "/newquote/:id",
+    element: <NewQuote />,
+  },
+  {
+    path: "/editquote/:id",
+    element: <EditQuote />,
+  },
+  {
+    path: "/editdraftquote/:id",
+    element: <EditDraftQuote />,
+  },
 ];
 
 const router = createBrowserRouter(routes);
