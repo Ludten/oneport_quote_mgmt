ec8447b642e1ee2c153f50092947a7b82a5a73d6
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2024-07-11 05:10:11 +0100

    updated types

--- src/types/AppType.ts
+++ src/types/AppType.ts
@@ -1,15 +1,25 @@
-import type { QuoteType } from "./QuoteType";
+import type { NewQuoteType, QuoteType } from "./QuoteType";
 
 interface AppType {
-    quotes: QuoteType[];
-    loading: boolean;
-    error: string | null;
+  quotes: QuoteType[];
+  loading: boolean;
+  error: string | null;
 }
 
 interface QuoteSliceType {
-    quote: QuoteType | null;
-    loading: boolean;
-    error: string | null;
+  quote: QuoteType | null;
+  loading: boolean;
+  error: string | null;
 }
 
-export type { AppType, QuoteSliceType }
+interface DraftQuotesSliceType {
+  quotes: NewQuoteType[];
+}
+
+interface CurrencyOption {
+  value: string;
+  flagCode: string;
+  prefix: string;
+}
+
+export type { AppType, QuoteSliceType, DraftQuotesSliceType, CurrencyOption };
