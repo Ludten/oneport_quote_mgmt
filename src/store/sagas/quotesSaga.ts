f374656629cadff2c44e6b8956cebeabe51a105a
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2024-07-11 05:10:58 +0100

    prettier changes

--- src/store/sagas/quotesSaga.ts
+++ src/store/sagas/quotesSaga.ts
@@ -1,12 +1,20 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
-import { getQuotes } from '../../helper/loader';
-import { fetchQuotesStart, fetchQuotesSuccess, fetchQuotesFailure } from '../slice/AppSlice';
-import type { QuotesResponse } from '../../types/ResponseType';
+import { takeLatest, call, put } from "redux-saga/effects";
+import { getQuotes } from "../../helper/loader";
+import {
+  fetchQuotesStart,
+  fetchQuotesSuccess,
+  fetchQuotesFailure,
+} from "../slice/AppSlice";
+import type { QuotesResponse } from "../../types/ResponseType";
 
 function* fetchQuotesSaga(action: ReturnType<typeof fetchQuotesStart>) {
   try {
     const { startDate, endDate } = action.payload;
-    const quotesResp: QuotesResponse = yield call(getQuotes, startDate, endDate);
+    const quotesResp: QuotesResponse = yield call(
+      getQuotes,
+      startDate,
+      endDate,
+    );
     yield put(fetchQuotesSuccess(quotesResp.data));
   } catch (err) {
     const error = err as Error;
