ac9ea080b7d756ae236ca051eaef7f7ce6187d25
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2024-07-11 05:14:53 +0100

    prettier

--- src/helper/serverops.ts
+++ src/helper/serverops.ts
@@ -1,63 +1,73 @@
-import { apiEndpoint } from '../config'
-import { NewQuoteType } from '../types/QuoteType';
-import type { QuoteResponse } from '../types/ResponseType'
+import { apiEndpoint } from "../config";
+import { NewQuoteType, RequestQuoteType } from "../types/QuoteType";
+import type { QuoteResponse } from "../types/ResponseType";
 
 export const createQuote = async (newQuote: NewQuoteType) => {
-    const uri = `${apiEndpoint}/create`;
+  const uri = `${apiEndpoint}/create`;
 
-    let ret: QuoteResponse = {
-        status: '',
-        data: null,
-        message: '',
-        code: 0
-    };
+  let ret: QuoteResponse = {
+    status: "",
+    data: null,
+    message: "",
+    code: 0,
+  };
 
-    try {
-      const res = await fetch(uri, {
-        method: 'POST',
-        headers: {
-          Accept: 'application.json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newQuote),
-      });
+  const reqQuote: RequestQuoteType = { ...newQuote };
 
-      const data = await res.json() as QuoteResponse;
-      ret = data;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-    
-    return ret
+  if ("id" in reqQuote) delete reqQuote["id"];
+  if ("endTime" in reqQuote) delete reqQuote["endTime"];
+
+  try {
+    const res = await fetch(uri, {
+      method: "POST",
+      headers: {
+        Accept: "application.json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reqQuote),
+    });
+
+    const data = (await res.json()) as QuoteResponse;
+    ret = data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+
+  return ret;
 };
 
 export const editQuote = async (id: string, updatedQuote: NewQuoteType) => {
-    const uri = `${apiEndpoint}/edit/${id}`;
+  const uri = `${apiEndpoint}/edit/${id}`;
 
-    let ret: QuoteResponse = {
-      status: '',
-      data: null,
-      message: '',
-      code: 0
-    };
+  let ret: QuoteResponse = {
+    status: "",
+    data: null,
+    message: "",
+    code: 0,
+  };
 
-    try {
-      const res = await fetch(uri, {
-        method: 'PUT',
-        headers: {
-          Accept: 'application.json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updatedQuote),
-      });
+  const reqQuote: RequestQuoteType = { ...updatedQuote };
 
-      const data = await res.json() as QuoteResponse;
-      ret = data;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
+  if ("id" in reqQuote) delete reqQuote["id"];
+  if ("endTime" in reqQuote) delete reqQuote["endTime"];
 
-    return ret;
+  try {
+    const res = await fetch(uri, {
+      method: "PUT",
+      headers: {
+        Accept: "application.json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reqQuote),
+    });
+
+    const data = (await res.json()) as QuoteResponse;
+    ret = data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+
+  return ret;
 };
